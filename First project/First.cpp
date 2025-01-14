0bca935b40c708c7a5726847be0a8c690bfd46ad
Zamanov <Zamanov@STHQ0125-01>
2025-01-14 16:57:53 +0400

    Add project files.

---
+++ First project/First.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+	cout << "Goodbye Python :((("<< endl;
+	cout << "Hello C++!!!"<< endl;
+	// single line comment
+	/*
+		multi line comments
+	  hot keys:
+		ctrl+k ctrl+c - comment
+		ctrl+k ctrl+u - uncomment	
+	*/
+	return 0;
+}
