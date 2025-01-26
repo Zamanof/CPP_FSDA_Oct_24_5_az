daa8dd8527ce7c690e0402ee18ba006723a13a3b
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-01-26 18:02:28 +0400

    for

---
+++ CPP 05. for/loops for.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	// for (start_value; condition; step)
+	/*for (int i = 0; i < 10; i++) {
+		cout << i << endl;
+	}*/
+
+	// 1.  int i = 0; for-un birinci hissesi bir defe 
+	// deyishen elan olunduqda ishleyir
+
+	// 2. i < 10;  for-un ikinci hissesi isleyir
+
+	// 3. for konstruksiyasinin body-si isleyir
+
+	// 4. i++ - for-un uchuncu hissesi isleyir
+	/*int i = 0;
+
+	for (;;) {
+		if (i == 10) break;
+		cout << i << endl;
+		i++;
+	}*/
+
+	/*int i = 0;
+
+	for (; i < 10; i++) {
+
+	}
+	cout << i << endl;*/
+
+
+}
