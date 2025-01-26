caf18a1f2902f6adc303fd19db83dd9e15b90159
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-01-26 15:57:37 +0400

    while, do while

---
+++ CPP 05. Loops while/Loops while.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	// Loops: while	
+
+	/*int i = 0;
+
+	while (i < 10) {
+		cout <<i<< ". Hello" << endl;
+		i++;
+	}
+	cout<<"End of code"<<endl;*/
+
+	// endless loop
+	/*int i = 0;
+	while (true) {
+		cout <<i<< ". Hello" << endl;
+		i++;
+	}*/
+
+	/*int i = 0;
+	while (i++ < 10) {
+		cout << i << ". Hello" << endl;
+	}*/
+
+
+	//int i = 10;
+	//// qeyri ashkar typecasting -> bool tipine
+	//while (i--) {
+	//	cout << i << ". Hello" << endl;
+	//}
+	//cout << i << endl;
+}
---
+++ CPP 05. do while/loops do while.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	// loops do while
+	/*int i = 0;
+	do {
+		cout << i << endl;
+		i++;
+	} while (i < 10);
+	cout << "End of code" << endl;*/
+
+
+	// while example
+	/*int number{};
+	cout << "Eded daxil edin:" << endl;
+	cin >> number;
+	cout << number << endl;
+	while (number != 7) {
+		cin >> number;
+		cout << "Eded daxil edin:" << endl;
+		cout << number << endl;
+	}
+
+	cout << "End" << endl;*/
+
+	// while example
+	/*int number{};
+	do {
+		cout << "Eded daxil edin:" << endl;
+		cin >> number;
+		cout << number << endl;
+	} while (number != 7);
+
+	cout << "End" << endl;*/
+}
+
