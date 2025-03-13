0244c4e5744a3fa0aa4553b7a45053cb51d33f55
moguda <zamanov@itstep.org>
2025-03-13 17:57:38 +0400

    OOP Intro

---
+++ CPP 23. OOP Intro/OOP intro.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+struct Student {
+	char name[50];
+	char surname[50];
+	int age;
+};
+
+class Human {
+public:
+	char name[50];
+	void show() {
+		cout << "Name :    " << name << endl;
+		cout << "Surname : " << surname << endl;
+		cout << "Age :     " << age << endl;
+	}
+private:
+	char surname[50];
+protected:
+	int age;
+};
+
+
+int main() {
+	// OOP - Object Oriented Paradigm (Programming) -> Obyekt yonumlu paradiqma
+	// C with classes -> C++
+
+	// OOP Principle:
+	// 1. Inheritance	- Toreme, vereselik
+	// 2. Encapsulation - Inkapsulyasiya
+	// 3. Polymorphism	- Polimorfizm
+	// 4. Abstraction	- Abstraksiya
+
+	// C++ da struct ve class arasinda yegane
+	// ferq default olaraq access modificator-un
+	// public(struc-da) ve ya private(class-da)
+	// omasidir
+
+	Student student;
+	student.age;
+	Human human;
+	human.name;
+	
+
+
+	// access modifiers
+	
+	// public		- publik, achiq-aydin (bu verilenler her kese elchatandir 
+	//					(class/struct daxilinde ve xaricinde))
+
+	// private		- privat, gizli (bu verilenler yalniz class/struct daxilinde
+	//					 elchatandir)
+
+	// protected	- qorunan (bu verilenler yalniz class/struct daxilinde ve
+	//					 vereselerde(ovladlarda) elchatandir)
+}
