2b0f655eca4269be942f7487a08ebc06360abb7c
moguda <zamanov@itstep.org>
2025-04-29 17:57:45 +0400

    i o stream

--- CPP 38. input output streams/input outpu stream.cpp
+++ CPP 38. input output streams/input outpu stream.cpp
@@ -10,4 +10,10 @@ int main() {
 	// input stream - informasiyanin oxunduqu stream(axin)
 	// output stream - informasiyanin yazildiqi stream(axin)
 	// two direction stream - informasiyanin hem yazilma hem de oxunmasini bacaran stream(axin)
+	
+	// cin - buffered
+	// cout - buffered
+	// cerr - out stream for errors (non buffered)  
+	// clog - out stream for errors (buffered)  
+
 }
