73dbe448789e1e34e9655d2c824d749395f378d1
moguda <zamanov@itstep.org>
2025-04-29 17:54:30 +0400

    iostream

---
+++ CPP 38. input output streams/input outpu stream.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int number{};
+	cin >> number;
+	cout << number << endl;
+
+	// input stream - informasiyanin oxunduqu stream(axin)
+	// output stream - informasiyanin yazildiqi stream(axin)
+	// two direction stream - informasiyanin hem yazilma hem de oxunmasini bacaran stream(axin)
+}
